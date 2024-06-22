77beb9a431c3d354093047cca79e8ae8a9d973d4
sarrthak008 <sarthaknavale08@gmail.com>
2024-06-22 07:25:50 +0530

    create script for render

--- app.js
+++ app.js
@@ -12,9 +12,31 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")))
 
  app.get('/',(req,res)=>{
-     res.render("index")
+     fs.readdir(`./hisabs`,(err,files)=>{
+         if(err){
+            res.send(err);
+         }else{
+            console.log(files)
+            res.render("index",{files:files})
+         }
+     })
  })
 
+ app.get('/create',(req,res)=>{
+         const  GetFileName=()=>{
+            const date = new Date();
+            let year = date.getFullYear()
+            let  month = date.getMonth()
+            let day = date.getDate()
+           return(`${year}-${month+1}-${day}`);
+         }
+    let Filename = GetFileName();
+    fs.writeFile(`./hisabs/${Filename}.txt`,"hiiii",(err)=>{
+         if(err)res.send(err)
+         else res.send("done")
+
+    })
+ })
 
 
  app.listen(PORT,()=>{
